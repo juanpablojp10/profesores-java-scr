6c8fd8bd48af30c20a26deffed9929d683605479
juanpablojp10 <juanpablojimenezp1@gmail.com>
2023-04-12 21:17:27 -0500

    se ingresa la base webpack

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('hola mundo ya la primera web');
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
-//  ↓ Sirve para identificar la ruta de donde se encuentra este archivo
+// Sirve para identificar la ruta de donde se encuentra este archivo
 const path = require('path');
 
-// Me permite trabajar con documentos html ↓
-const HtmlWebpackplugin = require('html-webpack-plugin');
+// Me permite trabajar con documentos html
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// ↓ Extraer los codigos css, minificarlo y optimizarlo, ademas lo agrega como parte del head
+// Extraer el código css, minificarlo y optimizarlo. Ademas lo agrega como parte del head
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// Nos permite copiar archivos de una ruta a otra ↓
-const CopywebpackPlugin = require('copy-webpack-plugin');
+// Nos permite copiar archivos de una ruta a otra
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
-
     const isProduction = argv.mode === 'production';
-
     return {
         entry: {
             index: './src/index.js',
+            styles: './src/styles.js'
         },
         output: {
-            filename: '[name].js',
+            filename: '[name].[contenthash].js',
             path: path.resolve(__dirname, 'dist')
         },
         module: {
             rules: [
-
                 {
                     test: /\.css$/,
                     use: [
@@ -33,18 +31,25 @@ module.exports = (env, argv) => {
                     ]
                 },
                 {
-                    text: /\.js$/,
+                    test: /\.js$/,
                     include: path.resolve(__dirname, 'src/assets/js'),
                     use: {
                         loader: 'babel-loader',
                         options: {
                             presets: ['@babel/preset-env']
                         }
+
                     }
                 }
             ]
         },
         plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                chunks: ['index','styles']
+            }),
+            // averiguar que significa un spread operator
+            ...(isProduction ? [new MiniCssExtractPlugin({ filename: 'assets/css/[name].[contenthash].css' })] : [])
         ],
         devServer: {
             static: {
@@ -57,5 +62,4 @@ module.exports = (env, argv) => {
             ]
         }
     };
-
 }
