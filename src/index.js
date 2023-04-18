2571c303e5f6fa200a5d3e5fad3b0065fa2804c2
juanpablojp10 <juanpablojimenezp1@gmail.com>
2023-04-17 21:25:04 -0500

    se termina el formulario

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+//ESTILOS ESPECIFICOS
+import './assets/CSS/index.css';
 console.log('hola mundo ya la primera web');
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,5 @@
+// ESTILOS GENERICOS
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
